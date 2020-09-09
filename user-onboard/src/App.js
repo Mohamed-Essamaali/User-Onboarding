a22be9a6895446902f646e8a2c8e3c4b2e13f839
Mohamed essamaali <mohamed.essamaali@gmail.com>
2020-09-09 19:06:28 -0400

    Added components users and styling

--- user-onboard/src/App.js
+++ user-onboard/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/form'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- user-onboard/src/components/form.js
+++ user-onboard/src/components/form.js
@@ -1,15 +1,21 @@
 import React,{useState,useEffect} from 'react';
 import * as yup from 'yup';
+import axios from 'axios';
+import {FormGroup,Label,Input,Col,Row} from 'reactstrap';
+import Users from './users'
 
 const Form = props=>{
     const[formData,setFormData] = useState({
         name:'',
         email:'',
         password:'',
-        terms:false
+        terms:false,
+        role:''
     })
 
     const [buttonDisabled,setButtonDisabled] = useState(true)
+
+    const[users,setUsers]=useState([])
 
     const handleChange = event=>{
     //     e.persist allows us to use the synthetic event in an async manner (inside of validateChange fn).
@@ -28,14 +34,17 @@ const Form = props=>{
         name:yup.string().required('Name is a required field').min(4,'Minimum four characters'),
         email: yup.string().email('Must enter a valid email').required('Must include email address'),
         password: yup.string().required('must provide a password').min(6,'password minimum 6 charaters'),
-        terms: yup.boolean().oneOf([true]).required('Please agree to terms of use')
+        terms: yup.boolean().oneOf([true],'Please agree to terms of use'),
+        role: yup.string()
     })
 
     const[errors,setErrors] = useState({
         name:'',
         email:'',
         password:'',
-        terms:false
+        terms:'',
+        role:''
+
     })
 
     useEffect(()=>{
@@ -51,44 +60,79 @@ const Form = props=>{
 
         yup
         .reach(formSchema,event.target.name)
-        .validate(event.target.name ==='checkbox'?event.target.checked:event.target.value)
+        .validate(event.target.type ==='checkbox'?event.target.checked:event.target.value)
         .then(valid=>{
-            setErrors({
-                ...errors,[event.target.name]:''
+            setErrors(
+                {...errors,[event.target.name]:''
             })
         }
            )
         .catch(err=>{
-            setErrors({
-                ...errors,[event.target.name]:err.errors[0]
+            setErrors(
+                {...errors,[event.target.name]:err.errors[0]
             })
         })
     }
-
+    const reset = ()=>{
+        setFormData({ name:'',
+        email:'',
+        password:'',
+        terms:false,
+    role:''})
+      
+    }
 
     const submitForm= event=>{
         event.preventDefault();
+        reset()
+        axios
+        .post('https://reqres.in/api/users',formData)
+        .then(res=>{
+            setUsers([...users,res.data])
+            console.log('form submitted',users)
+        })
 
     }
 
 
     return(
         <div className='form'>
-            <form onSubmit={submitForm}>
-                <label htmlFor='name'>Name</label>
-                <input id='name' type='string' name='name' value={formData.name} onChange={handleChange}/>
-                {errors.name.length>0?<p className='error'>{errors.name}</p>:null}
-                <label htmlFor='email'>Email</label>
-                <input id='email' type='email' name='email' value={formData.email} onChange={handleChange}/>
-                {errors.email.length>0?<p className='error'>{errors.email}</p>:null}
-                <label htmlFor='password'>Password</label>
-                <input id='password' type='password' name='password' value={formData.password} onChange={handleChange}/>
-                {errors.password.length>0?<p className='error'>{errors.password}</p>:null}
-                <label htmlFor = 'terms'>Agree the terms</label>
-                <input type='checkbox' id='terms' name='terms' checked={formData.terms} onChange={handleChange}/>
-                {errors.terms.length>0? <p className='error'>{errors.terms}</p> :null }
-                <button disabled = {buttonDisabled} type='submit'>Submit!</button>
+            <form onSubmit={submitForm} >
+               
+                
+                    <label  htmlFor='name'>Name</label>
+                    <input id='name' type='string' name='name' value={formData.name} onChange={handleChange}/>
+                    {errors.name.length>0?<p className='error'>{errors.name}</p>:null}
+               
+                    <label htmlFor='email'>Email</label>
+                    <input id='email' type='email' name='email' value={formData.email} onChange={handleChange}/>
+                    {errors.email.length>0?<p className='error'>{errors.email}</p>:null}
+               
+                    <label htmlFor='password'>Password</label>
+                    <input id='password' type='password' name='password' value={formData.password} onChange={handleChange}/>
+                    {errors.password.length>0?<p className='error'>{errors.password}</p>:null}
+                    <div className='terms-container'>
+                        <div className='terms'>
+                            <label htmlFor = 'terms'>Agree the terms</label>
+                            <input  type='checkbox' id='terms' name='terms' value={formData.terms?'agreed':'disagree'} checked={formData.terms} onChange={handleChange}/>
+                        </div>
+                     
+                        {<p className='error'>{errors.terms}</p> }
+                    </div>
+                  
+              
+                    <label htmlFor='role'>Role</label>
+                    <select id='role' name='role' onChange={handleChange}>
+                        <option value='frontEnd'>Front End Engineer </option>
+                        <option value='backEnd'>Back End Engineer </option>
+                        <option value='designer'>Designer </option>
+                    </select>
+             
+
+
+                    <button disabled = {buttonDisabled} type='submit'>Submit!</button>
             </form>
+            <Users users={users}/>
 
         </div>
     )
